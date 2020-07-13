e772e4a3e44eec3ea8576f3601ded29d7ddc25bc
Marcos Porto <marcosporto.sa@gmail.com>
2020-07-12 22:37:17 -0300

    For some reason the delete repository test doesnt pass even though it works on the browser

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
+
+
+
 function App() {
+
+
+  const [repositories, setRepositories] = useState([]);
+
+useEffect(()=>{
+  api.get('repositories').then( response => setRepositories(response.data));
+
+}, []);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {  
+      title: `Novo repositório ${Date.now()}`,
+      url:`https://github.com/josepholiveira/${Date.now()}.git`,
+      techs:["React","Node"],
+    })
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    console.log(repositories);
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const response = await api.get('repositories');
+    setRepositories(response.data);
+    console.log(response.data);
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
-
+            
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
